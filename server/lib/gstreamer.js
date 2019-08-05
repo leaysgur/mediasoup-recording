cec2082978c72ce7ffac812636072c2c34c92fdd
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-08-05 19:10:29 +0900

    Cosme

--- server/lib/gstreamer.js
+++ server/lib/gstreamer.js
@@ -6,6 +6,7 @@ module.exports = (port, pt, dest) => {
   const cmd = "gst-launch-1.0";
   // debug: echo back
   // const opts = `rtpbin name=rtpbin udpsrc port=${port} caps="application/x-rtp,media=audio,clock-rate=48000,encoding-name=OPUS,payload=${pt}" ! rtpbin.recv_rtp_sink_0 rtpbin. ! rtpopusdepay ! opusdec ! autoaudiosink`;
+
   // debug: save .wav
   const opts = `rtpbin name=rtpbin udpsrc port=${port} caps="application/x-rtp,media=audio,clock-rate=48000,encoding-name=OPUS,payload=${pt}" ! rtpbin.recv_rtp_sink_0 rtpbin. ! rtpopusdepay ! opusdec ! wavenc ! filesink location=${dest}`;
 
